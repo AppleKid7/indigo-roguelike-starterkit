df4e3874f88be92cc2dcf519d1e246f0d28ab10e
Javier <kvltd00d@gmail.com>
2021-08-03 11:56:47 -0400

    cleanup for part 5

--- src/main/scala/model/GameMap.scala
+++ src/main/scala/model/GameMap.scala
@@ -2,7 +2,7 @@ package roguelike.model
 
 import indigo._
 import indigo.lib.roguelike.terminal.MapTile
-import indigo.lib.roguelike.utils.MapUtils.calculateFOV
+import indigo.lib.roguelike.utils.MapUtils.bresenhamLine
 import indigo.lib.roguelike.DfTiles
 
 import indigoextras.trees.QuadTree
@@ -20,7 +20,7 @@ final case class GameMap(size: Size, tileMap: QuadTree[GameTile], visible: List[
       case Some(tile) => tm.insertElement(tile, vtx)
 
   def update(playerPosition: Point): GameMap =
-    val newVisible = calculateFOV[GameTile](15, playerPosition, tileMap)
+    val newVisible = GameMap.calculateFOV(15, playerPosition, tileMap)
     this.copy(
       visible = newVisible,
       explored = explored ++ newVisible
@@ -90,70 +90,41 @@ object GameMap:
   def generateMap(size: Size, dungeon: Dungeon): GameMap =
     initial(size).insert(dungeon.positionedTiles)
 
-  // def calculateFOV(radius: Int, center: Point, tileMap: QuadTree[GameTile]): List[Point] =
-  //   val bounds: Rectangle =
-  //     Rectangle(
-  //       (center - radius).max(0),
-  //       (Size(center.x, center.y) + radius).max(1)
-  //     )
-  //   val tiles =
-  //     searchByBounds(tileMap, bounds).filter(pt => center.distanceTo(pt) <= radius)
+  def calculateFOV(radius: Int, center: Point, tileMap: QuadTree[GameTile]): List[Point] =
+    val bounds: Rectangle =
+      Rectangle(
+        (center - radius).max(0),
+        (Size(center.x, center.y) + radius).max(1)
+      )
+    val tiles =
+      searchByBounds(tileMap, bounds).filter(pt => center.distanceTo(pt) <= radius)
 
-  //   @tailrec
-  //   def visibleTiles(remaining: List[Point], acc: List[Point]): List[Point] =
-  //     remaining match
-  //       case Nil => acc
-  //       case pt :: pts =>
-  //         val lineOfSight = bresenhamLine(pt, center)
-  //         if lineOfSight.forall(tiles.contains) then
-  //           visibleTiles(
-  //             pts,
-  //             pt :: acc
-  //           )
-  //         else visibleTiles(pts, acc)
-  //   visibleTiles(tiles, Nil)
+    @tailrec
+    def visibleTiles(remaining: List[Point], acc: List[Point]): List[Point] =
+      remaining match
+        case Nil => acc
+        case pt :: pts =>
+          val lineOfSight = bresenhamLine(pt, center)
+          if lineOfSight.forall(tiles.contains) then
+            visibleTiles(
+              pts,
+              pt :: acc
+            )
+          else visibleTiles(pts, acc)
+    visibleTiles(tiles, Nil)
 
-  // def searchByBounds(quadTree: QuadTree[GameTile], bounds: Rectangle): List[Point] =
-  //   val boundingBox: BoundingBox = BoundingBox.fromRectangle(bounds)
+  def searchByBounds(quadTree: QuadTree[GameTile], bounds: Rectangle): List[Point] =
+    val boundingBox: BoundingBox = BoundingBox.fromRectangle(bounds)
 
-  //   @tailrec
-  //   def rec(remaining: List[QuadTree[GameTile]], acc: List[Point]): List[Point] =
-  //     remaining match
-  //       case Nil => acc
-  //       case x :: xs =>
-  //         x match
-  //           case QuadBranch(bounds, a, b, c, d) if boundingBox.overlaps(bounds) =>
-  //             rec(a :: b :: c :: d :: xs, acc)
-  //           case QuadLeaf(_, exactPosition, value) if boundingBox.contains(exactPosition) =>
-  //             rec(xs, exactPosition.toPoint :: acc)
-  //           case _ => rec(xs, acc)
-  //   rec(List(quadTree), Nil)
-
-  // def bresenhamLine(from: Point, to: Point): List[Point] =
-  //   val x0: Int = from.x
-  //   val y0: Int = from.y
-  //   val x1: Int = to.x
-  //   val y1: Int = to.y
-  //   val dx = Math.abs(x1 - x0)
-  //   val dy = Math.abs(y1 - y0)
-  //   val sx = if x0 < x1 then 1 else -1
-  //   val sy = if y0 < y1 then 1 else -1
-
-  //   @tailrec
-  //   def rec(x: Int, y: Int, err: Int, acc: List[Point]): List[Point] =
-  //     val next = Point(x, y)
-  //     if (x == x1 && y == y1) || next.distanceTo(to) <= 1 then next :: acc
-  //     else
-  //       var e = err
-  //       var x2 = x
-  //       var y2 = y
-
-  //       if err > -dx then
-  //         e -= dy
-  //         x2 += sx
-  //       if e < dy then
-  //         e += dx
-  //         y2 += sy
-  //       rec(x2, y2, e, next :: acc)
-
-  //   rec(x0, y0, (if dx > dy then dx else -dy) / 2, Nil)
+    @tailrec
+    def rec(remaining: List[QuadTree[GameTile]], acc: List[Point]): List[Point] =
+      remaining match
+        case Nil => acc
+        case x :: xs =>
+          x match
+            case QuadBranch(bounds, a, b, c, d) if boundingBox.overlaps(bounds) =>
+              rec(a :: b :: c :: d :: xs, acc)
+            case QuadLeaf(_, exactPosition, value) if boundingBox.contains(exactPosition) =>
+              rec(xs, exactPosition.toPoint :: acc)
+            case _ => rec(xs, acc)
+    rec(List(quadTree), Nil)
